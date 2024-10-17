62bc27e6aa86bd64cdebd5a86401ce21f3d64c6f
Michelle <l23200936@pachuca.tecnm.mx>
2024-10-17 14:51:58 -0600

    Mi primera App

---
+++ main.py
@@ -0,0 +1,14 @@
+from itertools import filterfalse
+
+from classWindow import Window
+
+ventana = Window()
+ventana.setDimensions(500,500)
+ventana.setTitulo("Mi primera App")
+ventana.setResize(False)
+
+# print(ventana.getDimensions())
+# print(ventana.getResize())
+# print(ventana.getTitulo())
+
+ventana.crear_ventana()
